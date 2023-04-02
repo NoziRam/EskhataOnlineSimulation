0acaf0a28411b09ca1d330b8c1e57d5f9e3d46c1
NoziRam <1000dreams000@gmail.com>
2023-04-02 11:57:18 +0500

    added MVC classes

--- EskhataOnlineSimulation/Startup.cs
+++ EskhataOnlineSimulation/Startup.cs
@@ -12,10 +12,18 @@ namespace EskhataOnlineSimulation
 {
     public class Startup
     {
+        public Startup()
+        {
+
+        }
+        
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(options=>options.EnableEndpointRouting=false);
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +34,8 @@ namespace EskhataOnlineSimulation
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
