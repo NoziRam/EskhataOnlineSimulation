0765a823ef645028be15283ee67faa6ffd4a854a
NoziRam <1000dreams000@gmail.com>
2023-04-02 11:25:11 +0500

    added properties class Card

--- EskhataOnlineSimulation/Models/Card.cs
+++ EskhataOnlineSimulation/Models/Card.cs
@@ -7,6 +7,14 @@ namespace EskhataOnlineSimulation.Models
 {
     public class Card
     {
+        public int Id { get; set; }
+
+        public int Name { get; set; }
+
+        public int Account { get; set; }
+
+        public bool Status { get; set; }
+
 
     }
 }
